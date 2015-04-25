31fe846c96404356abb7158b2f7db5e20932b20c
Alex Brandt <alunduil@alunduil.com>
2015-04-24 21:11:01 -0500

    use Snap

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-main = do {
-	putStrLn "Hello World!"
-}
+import Control.Applicative()
+import Snap.Core
+import Snap.Util.FileServe()
+import Snap.Http.Server
+
+main :: IO ()
+main = quickHttpServe site
+
+site :: Snap ()
+site = route [ ( ":urlid", urlHandler ) ]
+
+urlHandler :: Snap ()
+urlHandler = do
+    urlid <- getParam "urlid"
+    maybe ( writeBS "must specify an ID" )
+          writeBS urlid
